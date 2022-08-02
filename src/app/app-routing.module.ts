a0e1567092a8bd0e73fe519f885470660717e9b4
atstahir <atstahir@outlook.com>
2022-08-03 00:15:22 +0500

    added links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './core/components/products/products.component';
+import { UsersComponent } from './core/components/users/users.component';
 
 const routes: Routes = [
-  { path:'products', component: ProductsComponent }
+  { path:'products', component: ProductsComponent },
+  { path:'users', component:UsersComponent }
 ];
 
 @NgModule({
